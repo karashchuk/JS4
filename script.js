f6206d3f09d16c7107b1ccb897de0b7aef0ea8ef
pantol <pantolik@rambler.ru>
2016-04-04 20:46:56 +0300

    Заменил window.onload на $(function). и причесал программу

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-
-window.onload = function(){
-
-    //$('.articles').hide();
-    $('div.active').show();
-    $('#list').on('click','li',function(){
-    //$('li').click( function(){
-        $('li.active').removeClass('active');
-        $(this).addClass('active');
-        $('div.active').removeClass('active').hide();
-        $('div.articles').eq($(this).index()).addClass('active').show();
+(function($){
+    $(function(){
         //$('.articles').hide();
-        //$('div.active').show();
+        $('div.active').show();
+        $('#list').on('click','li',function(){
+            $('li.active').removeClass('active');
+            $(this).addClass('active');
+            $('div.active').removeClass('active').hide();
+            $('div.articles').eq($(this).index()).addClass('active').show();
+        });
     });
-    
-}
+})(jQuery);
